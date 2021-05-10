96965c7e2425f68f923e9980724e08866d227744
Nacho Gómez <nachote01@gmail.com>
2021-05-10 11:19:41 +0200

    queue.c comentado

--- queue.c
+++ queue.c
@@ -8,11 +8,11 @@ int numelems = 0;
 
 //To create a queue
 queue* queue_init(int size){
-    queue * q = (queue *)malloc(sizeof(queue));
+    queue * q = (queue *)malloc(sizeof(queue)); //Reserve memory for the queue
     q->_size = size; //Set size
     q->_elems = (struct element*) malloc(sizeof(struct element) * size); //Allocate memory for elements
-    q->_inpos = 0; //inpos = 0
-    q->_outpos = 0; //outpos = 0
+    q->_inpos = 0; //The attribute inpos will be the index of the element to be enqueued
+    q->_outpos = 0; //outpos will be the index of the element to be dequeued
     q->_numelems = 0; //Free queue
     return q;
 }
@@ -21,31 +21,29 @@ queue* queue_init(int size){
 // To Enqueue an element
 int queue_put(queue *q, struct element* x) {
     if (!queue_full(q)){
-        q->_elems[q->_inpos] = *x;
-        q->_inpos = (q->_inpos + 1) % q->_size;
-        q->_numelems++;
+        q->_elems[q->_inpos] = *x; //Insert the element in the queue to the input index
+        q->_inpos = (q->_inpos + 1) % q->_size; //Update the index value (if we are in the end we must go to first position)  
+        q->_numelems++; //Increment the queue size
         return 0;
     } else {
         printf("Cannot queue_put: queue is full\n");
         return -1;
     }
-    /*HACER EL WAIT PARA CUANDO ESTÁ LLENO?*/
 }
 
 
 // To Dequeue an element.
 struct element* queue_get(queue *q) {
-    struct element* element = NULL;
+    struct element* element = NULL; //Initialize an empty struct* element to save result
     if (!queue_empty(q)){
-        element = &(q->_elems[q->_outpos]);
-        q->_outpos = (q->_outpos + 1) % q->_size;
-        q->_numelems--;
+        element = &(q->_elems[q->_outpos]); //Get the address of the element to be dequeued
+        q->_outpos = (q->_outpos + 1) % q->_size; //Update outpos index
+        q->_numelems--; //Decrease queue size
         return element;
     } else {
         printf("Cannot queue_get: queue is empty\n");
         return NULL;
     }
-    /*HACER EL WAIT PARA CUANDO ESTÁ VACÍO?*/
 }
 
 
